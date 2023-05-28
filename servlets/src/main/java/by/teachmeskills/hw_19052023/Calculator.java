c6e4d58887de705163e482100fce7433c07fff8a
ShylkoVad <shylkovad@gmail.com>
2023-05-28 11:47:21 +0300

    hw_19052023 (28/05/2023 11:46)

---
+++ servlets/src/main/java/by/teachmeskills/hw_19052023/Calculator.java
@@ -0,0 +1,23 @@
+package by.teachmeskills.hw_19052023;
+
+public class Calculator {
+
+    public Calculator() {
+    }
+
+    public static double sum(double number1, double number2) {
+        return number1 + number2;
+    }
+
+    public static double substr(double number1, double number2) {
+        return number1 - number2;
+    }
+
+    public static double multiply(double number1, double number2) {
+        return number1 * number2;
+    }
+
+    public static double div(double number1, double number2) {
+        return number1 / number2;
+    }
+}
---
+++ servlets/src/main/java/by/teachmeskills/hw_19052023/MainServlet.java
@@ -0,0 +1,44 @@
+package by.teachmeskills.hw_19052023;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/calculator")
+public class MainServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+        private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        String number1 = request.getParameter("number1");
+        String number2 = request.getParameter("number2");
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/index.jsp");
+        String operation = request.getParameterValues("operation")[0];
+        if (operation != null) {
+            switch (operation) {
+                case "sumOperation" ->
+                        request.setAttribute("result", Calculator.sum(Double.parseDouble(number1), Double.parseDouble(number2)));
+                case "substrOperation" ->
+                        request.setAttribute("result", Calculator.substr(Double.parseDouble(number1), Double.parseDouble(number2)));
+                case "multiplyOperation" ->
+                        request.setAttribute("result", Calculator.multiply(Double.parseDouble(number1), Double.parseDouble(number2)));
+                case "divOperation" ->
+                        request.setAttribute("result", Calculator.div(Double.parseDouble(number1), Double.parseDouble(number2)));
+            }
+        }
+        requestDispatcher.forward(request, response);
+    }
+}
